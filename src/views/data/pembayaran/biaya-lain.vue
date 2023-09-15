6de024e659ee29a8725754b434856a7f768b8147
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-09-15 10:26:20 +0700

    add total terbayar + discount :D

--- src/views/data/pembayaran/biaya-lain.vue
+++ src/views/data/pembayaran/biaya-lain.vue
@@ -1,149 +1,148 @@
 <script setup>
-	import { onMounted, reactive, watchEffect } from 'vue';
-	import Card from '../../../components/Card.vue';
-	import { http, notify, formatMoney, formatDate, auth } from '../../../lib';
-	import Table from '../../../components/Table.vue';
-	import Modal from '../../../components/Modal.vue';
-	import Loading from '../../../components/Loading.vue';
+import { onMounted, reactive, watchEffect } from 'vue';
+import Card from '../../../components/Card.vue';
+import { http, notify, formatMoney, formatDate, auth } from '../../../lib';
+import Table from '../../../components/Table.vue';
+import Modal from '../../../components/Modal.vue';
+import Loading from '../../../components/Loading.vue';
 
-	const props = defineProps(['siswa']);
-	const state = reactive({
-		isLoading: false,
-		isDelete: false,
-		data: {
-			items: [],
-		},
-		biaya_lain: [],
-		terbayar: [],
-		modal: {
-			save: false,
-		},
-		req: {
-			id: '',
-			siswa_nisn: '',
-			user_id: auth.id,
-			biaya_lain_id: '',
-			jumlah_bayar: '',
-		},
-		delete: {},
-		item: {},
-	});
+const props = defineProps(['siswa']);
+const state = reactive({
+	isLoading: false,
+	isDelete: false,
+	data: {
+		items: [],
+	},
+	biaya_lain: [],
+	terbayar: [],
+	terbayarTotal: 0,
+	modal: {
+		save: false,
+	},
+	req: {
+		id: '',
+		siswa_nisn: '',
+		user_id: auth.id,
+		biaya_lain_id: '',
+		jumlah_bayar: '',
+	},
+	delete: {},
+	item: {},
+});
 
-	function getPembayaran() {
+function getPembayaran() {
+	http
+		.get('/pembayaran/biaya-lain', {
+			_siswa_nisn: props.siswa.nisn,
+			_biaya_lain_id: state.req.biaya_lain_id,
+		})
+		.then((res) => res.json())
+		.then((res) => {
+			state.data.items = res;
+		});
+}
+
+function get() {
+	http
+		.get('/pembayaran/biaya-lain', {
+			_siswa_nisn: props.siswa.nisn,
+		})
+		.then((res) => res.json())
+		.then((res) => {
+			state.terbayar = [];
+			state.terbayarTotal = 0;
+			res.forEach((item) => {
+				if (state.terbayar[item.biaya_lain_id]) {
+					state.terbayar[item.biaya_lain_id] += item.jumlah_bayar;
+					state.terbayarTotal += item.jumlah_bayar;
+				} else {
+					state.terbayar[item.biaya_lain_id] = item.jumlah_bayar;
+				}
+			});
+			state.isLoading = false;
+		});
+}
+
+function getData() {
+	state.isLoading = true;
+	if (props.siswa.nisn) {
 		http
-			.get('/pembayaran/biaya-lain', {
-				_siswa_nisn: props.siswa.nisn,
-				_biaya_lain_id: state.req.biaya_lain_id,
+			.get('/biaya-lain', {
+				_jurusan_kode: props.siswa.jurusan.kode,
+				_kelas: props.siswa.kelas,
 			})
 			.then((res) => res.json())
 			.then((res) => {
-				state.data.items = res;
+				state.biaya_lain = res;
+				get();
+			})
+			.catch((err) => {
+				notify(err);
 			});
 	}
+}
 
-	function get() {
-		http
-			.get('/pembayaran/biaya-lain', {
-				_siswa_nisn: props.siswa.nisn,
-			})
-			.then((res) => res.json())
-			.then((res) => {
-				state.terbayar = [];
-				res.forEach((item) => {
-					if (state.terbayar[item.biaya_lain_id]) {
-						state.terbayar[item.biaya_lain_id] += item.jumlah_bayar;
-					} else {
-						state.terbayar[item.biaya_lain_id] = item.jumlah_bayar;
-					}
-				});
-				state.isLoading = false;
-			});
+function store(e = {}, fn = false) {
+	if (fn) {
+		fn();
 	}
 
-	function getData() {
-		state.isLoading = true;
-		if (props.siswa.nisn) {
-			http
-				.get('/biaya-lain', {
-					_jurusan_kode: props.siswa.jurusan.kode,
-					_kelas: props.siswa.kelas,
-				})
-				.then((res) => res.json())
-				.then((res) => {
-					state.biaya_lain = res;
-					get();
-				})
-				.catch((err) => {
-					notify(err);
-				});
-		}
-	}
-
-	function store(e = {}, fn = false) {
-		if (fn) {
-			fn();
-		}
-
-		http
-			.post('/pembayaran/biaya-lain', state.req)
-			.then((res) => res.json())
-			.then((res) => {
-				new Promise((resolve) => {
-					notify('pembayaran berhasil disimpan');
-					state.req.max_jumlah_bayar -= state.req.jumlah_bayar;
-					state.req.jumlah_bayar = '';
-					if (e) {
-						e[0].value = '';
-					}
-					resolve(true);
-				}).then(() => {
-					getData();
-					getPembayaran();
-				});
-			})
-			.catch((err) => notify(err, 'bg-red-400'));
-	}
-
-	function destroy() {
-		const item = state.delete;
-
-		http
-			.delete('/pembayaran/biaya-lain/' + item.id)
-			.then((res) => res.json())
-			.then((res) => {
-				notify('pembayaran berhasil dihapus');
-				state.req.max_jumlah_bayar += item.jumlah_bayar;
+	http
+		.post('/pembayaran/biaya-lain', state.req)
+		.then((res) => res.json())
+		.then((res) => {
+			new Promise((resolve) => {
+				notify('pembayaran berhasil disimpan');
+				state.req.max_jumlah_bayar -= state.req.jumlah_bayar;
 				state.req.jumlah_bayar = '';
-				state.isDelete = false;
+				if (e) {
+					e[0].value = '';
+				}
+				resolve(true);
+			}).then(() => {
 				getData();
 				getPembayaran();
-			})
-			.catch((err) => notify(err, 'bg-red-400'));
-	}
+			});
+		})
+		.catch((err) => notify(err, 'bg-red-400'));
+}
 
-	watchEffect(() => {
-		if (props.siswa) getData();
-	});
+function destroy() {
+	const item = state.delete;
+
+	http
+		.delete('/pembayaran/biaya-lain/' + item.id)
+		.then((res) => res.json())
+		.then((res) => {
+			notify('pembayaran berhasil dihapus');
+			state.req.max_jumlah_bayar += item.jumlah_bayar;
+			state.req.jumlah_bayar = '';
+			state.isDelete = false;
+			getData();
+			getPembayaran();
+		})
+		.catch((err) => notify(err, 'bg-red-400'));
+}
+
+watchEffect(() => {
+	if (props.siswa) getData();
+});
 </script>
 
 <template>
 	<Card title="Biaya Lain">
 		<div class="relative">
 			<Loading v-if="state.isLoading"></Loading>
-			<Table
-				class="mt-3"
-				:keys="{
-					Pembayaran: 'jenis',
-					Jumlah: 'jumlah_bayar',
-					Diskon: 'diskon',
-					Total_Bayar: 'total_bayar',
-					Terbayar: 'terbayar',
-					Belum_Terbayar: 'belum_terbayar',
-					Bayar: 'bayar',
-					Opsi: 'opsi',
-				}"
-				:items="state.biaya_lain"
-			>
+			<Table class="mt-3" :keys="{
+				Pembayaran: 'jenis',
+				Jumlah: 'jumlah_bayar',
+				Diskon: 'diskon',
+				Total_Bayar: 'total_bayar',
+				Terbayar: 'terbayar',
+				Belum_Terbayar: 'belum_terbayar',
+				Bayar: 'bayar',
+				Opsi: 'opsi',
+			}" :items="state.biaya_lain">
 				<!-- <template #jenis="{ item }">
 					{{ item.jenis }} - {{ item.created_at.slice(0, 4) }}
 				</template> -->
@@ -170,70 +169,54 @@
 					{{
 						formatMoney(
 							item.jumlah_bayar -
-								(item.jumlah_bayar * props.siswa.diskon_biaya_lain) / 100 -
-								(state.terbayar[item.id] ? state.terbayar[item.id] : 0),
+							(item.jumlah_bayar * props.siswa.diskon_biaya_lain) / 100 -
+							(state.terbayar[item.id] ? state.terbayar[item.id] : 0),
 						)
 					}}
 				</template>
 
 				<template #bayar="{ item }">
-					<form
-						@submit.prevent="
-							store($event.currentTarget, () => {
-								state.req.biaya_lain_id = item.id;
-								state.req.siswa_nisn = props.siswa.nisn;
-								state.req.jumlah_bayar = $event.currentTarget[0].value;
-							})
-						"
-						class="inline"
-					>
-						<input
-							type="number"
-							:placeholder="
-								formatMoney(
-									item.jumlah_bayar -
-										(item.jumlah_bayar * props.siswa.diskon_biaya_lain) / 100 -
-										(state.terbayar[item.id] ? state.terbayar[item.id] : 0),
-								)
-							"
-							min="1"
-							:max="
-								item.jumlah_bayar -
-								(item.jumlah_bayar * props.siswa.diskon_biaya_lain) / 100 -
-								(state.terbayar[item.id] ? state.terbayar[item.id] : 0)
-							"
-							:disabled="
-								item.jumlah_bayar -
-									(item.jumlah_bayar * props.siswa.diskon_biaya_lain) / 100 -
-									(state.terbayar[item.id] ? state.terbayar[item.id] : 0) <=
-								0
-							"
-							required
-							class="h-10 rounded border-gray-100 text-gray-500 shadow outline-none w-120px disabled:bg-gray-100 focus:invalid:(ring-2 ring-red-500)"
-						/>
+					<form @submit.prevent="
+						store($event.currentTarget, () => {
+							state.req.biaya_lain_id = item.id;
+							state.req.siswa_nisn = props.siswa.nisn;
+							state.req.jumlah_bayar = $event.currentTarget[0].value;
+						})
+						" class="inline">
+						<input type="number" :placeholder="formatMoney(
+							item.jumlah_bayar -
+							(item.jumlah_bayar * props.siswa.diskon_biaya_lain) / 100 -
+							(state.terbayar[item.id] ? state.terbayar[item.id] : 0),
+						)
+							" min="1" :max="item.jumlah_bayar -
+		(item.jumlah_bayar * props.siswa.diskon_biaya_lain) / 100 -
+		(state.terbayar[item.id] ? state.terbayar[item.id] : 0)
+		" :disabled="item.jumlah_bayar -
+		(item.jumlah_bayar * props.siswa.diskon_biaya_lain) / 100 -
+		(state.terbayar[item.id] ? state.terbayar[item.id] : 0) <=
+		0
+		" required
+							class="h-10 rounded border-gray-100 text-gray-500 shadow outline-none w-120px disabled:bg-gray-100 focus:invalid:(ring-2 ring-red-500)" />
 						<button type="submit" class="hidden"></button>
 					</form>
 				</template>
 
 				<template #opsi="{ item }">
 					<div class="bg-white rounded inline-block px-1">
-						<button
-							@click="
-								state.item = JSON.parse(JSON.stringify(item));
-								state.modal.save = true;
+						<button @click="
+							state.item = JSON.parse(JSON.stringify(item));
+						state.modal.save = true;
 
-								state.req.jumlah_bayar = '';
-								state.req.biaya_lain_id = item.id;
-								state.req.siswa_nisn = props.siswa.nisn;
-								state.req.max_jumlah_bayar =
-									item.jumlah_bayar -
-									(item.jumlah_bayar * props.siswa.diskon_biaya_lain) / 100 -
-									(state.terbayar[item.id] ? state.terbayar[item.id] : 0);
-								state.data.items = [];
-								getPembayaran();
-							"
-							class="material-icons-outlined text-blue-500 !text-xl"
-						>
+						state.req.jumlah_bayar = '';
+						state.req.biaya_lain_id = item.id;
+						state.req.siswa_nisn = props.siswa.nisn;
+						state.req.max_jumlah_bayar =
+							item.jumlah_bayar -
+							(item.jumlah_bayar * props.siswa.diskon_biaya_lain) / 100 -
+							(state.terbayar[item.id] ? state.terbayar[item.id] : 0);
+						state.data.items = [];
+						getPembayaran();
+						" class="material-icons-outlined text-blue-500 !text-xl">
 							edit
 						</button>
 					</div>
@@ -252,8 +235,7 @@
 								<div class="oveflow-x-auto bg-white mb-4 rounded p-2 relative">
 									<div
 										class="absolute bottom-5 px-4 right-5 transform p-1 -rotate-12 border-2 border-purple-500 text-purple-500 rounded"
-										v-if="state.req.max_jumlah_bayar <= 0"
-									>
+										v-if="state.req.max_jumlah_bayar <= 0">
 										LUNAS
 									</div>
 									<table>
@@ -266,11 +248,8 @@
 											<td>Terbayar</td>
 											<td class="px-2">:</td>
 											<td class="text-green-500">
-												{{
-													formatMoney(
-														state.terbayar[state.item.id] ? state.terbayar[state.item.id] : 0,
-													)
-												}}
+												{{ formatMoney((state.terbayar[state.item.id] || 0) + props.siswa.diskon_biaya_lain / 100 *
+													state.item.jumlah_bayar) }}
 											</td>
 										</tr>
 										<tr>
@@ -281,15 +260,8 @@
 									</table>
 								</div>
 								<label for="">Nominal</label>
-								<input
-									type="number"
-									v-model="state.req.jumlah_bayar"
-									placeholder="Masukkan Nominal"
-									min="1"
-									:max="state.req.max_jumlah_bayar"
-									:disabled="state.req.max_jumlah_bayar <= 0"
-									required
-								/>
+								<input type="number" v-model="state.req.jumlah_bayar" placeholder="Masukkan Nominal" min="1"
+									:max="state.req.max_jumlah_bayar" :disabled="state.req.max_jumlah_bayar <= 0" required />
 							</div>
 						</div>
 						<div class="rounded-b text-center py-1 px-4 text-blue-500" :hidden="state.req.max_jumlah_bayar <= 0">
@@ -297,10 +269,7 @@
 						</div>
 					</form>
 				</div>
-				<div
-					class="bg-red-100 mb-4 border rounded border-red-500 text-red-500 p-3 form-field"
-					v-if="state.isDelete"
-				>
+				<div class="bg-red-100 mb-4 border rounded border-red-500 text-red-500 p-3 form-field" v-if="state.isDelete">
 					<div class="lg:flex">
 						<div class="w-full flex items-center">Anda yakin menghapus data terpilih?</div>
 						<div class="w-full lg:text-right text-center lg:mt-0 mt-4">
@@ -322,14 +291,11 @@
 						</div>
 						<div class="rounded-b p-1 text-center flex justify-between px-4 items-center h-10">
 							<a :href="item.print_url" target="_blank" class="mr-2 text-sm">PRINT</a>
-							<button
-								@click="
-									state.isDelete = true;
-									state.delete = item;
-								"
-								class="material-icons-outlined text-red-500 !text-xl"
-								v-if="item.user_id == auth.id ? true : auth.role == '1' ? true : false"
-							>
+							<button @click="
+								state.isDelete = true;
+							state.delete = item;
+							" class="material-icons-outlined text-red-500 !text-xl"
+								v-if="item.user_id == auth.id ? true : auth.role == '1' ? true : false">
 								delete
 							</button>
 						</div>
